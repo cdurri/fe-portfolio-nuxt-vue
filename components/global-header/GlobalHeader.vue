a54b16480d10f633946500d81faa3fcad263ac37
ciandurrigan <ciand>
2020-03-28 21:00:32 +0000

    Add navbar brand and image file for global header component

--- components/global-header/GlobalHeader.vue
+++ components/global-header/GlobalHeader.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-        Global Header
-    </div>
+    <nav class="navbar has-background-white-bis" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+                <figure class="image is-96x96">
+                    <img class="is-rounded" src="~/assets/cian_icon_bw.jpg" height="96">
+                </figure>
+            </a>
+        </div>
+    </nav>
 </template>
 
-<style>
-
+<style scoped lang="scss">
+    .navbar-item {
+        img {
+            max-height: 96px;
+        }
+    }
 </style>
